eaaf0f10bb387fc6c5cc27180b58359c17f55589
Håkon Ødegård Løvdal <post@hakloev.no>
2019-03-18 21:52:48 +0100

    Enable CORS on Entur-endpoint

--- backend/src/main/kotlin/com/hakloev/screen/controller/EnturController.kt
+++ backend/src/main/kotlin/com/hakloev/screen/controller/EnturController.kt
@@ -3,15 +3,13 @@ package com.hakloev.screen.controller
 import com.hakloev.screen.api.entur.EnturApi
 import com.hakloev.screen.model.entur.DeparturesResponse
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/entur")
 class EnturController(private val enturApi: EnturApi) {
 
+    @CrossOrigin
     @GetMapping("/departures", produces = [MediaType.APPLICATION_JSON_UTF8_VALUE])
     fun getDepartures(
             @RequestParam("stopPlace") stopPlace: String,
