d91dcb28a48d05e285270e81365df1c0ecc0ff6d
Håkon Ødegård Løvdal <post@hakloev.no>
2019-03-24 19:56:19 +0100

    Dont fetch when tab not visible. Not happy with fetching logic and need to do something about it

--- frontend/src/containers/departures/Departures.tsx
+++ frontend/src/containers/departures/Departures.tsx
@@ -10,11 +10,8 @@ interface IDeparturesState {
     isLoading: boolean;
     platforms: IPlatform[];
     lastUpdated?: Date;
-    time: Date;
+    updateUiTimestamp: Date;
 }
-
-const FETCH_DEPARTURES_INTERVAL = 30_000;
-const UPDATE_UI_INTERVAL = 5_000;
 
 const Container = styled.section`
     display: flex;
@@ -27,48 +24,61 @@ const LastUpdated = styled.p`
     margin: 0;
 `;
 
+const FETCH_DEPARTURES_INTERVAL = 60_000;
+const UPDATE_UI_INTERVAL = 5_000;
+
 class Departures extends React.PureComponent<{}, IDeparturesState> {
+
     fetchDeparturesInterval?: number;
-    updateDeparturesInterval?: number;
+    updateUiInterval?: number;
 
     state: IDeparturesState = {
         isLoading: true,
         lastUpdated: undefined,
         platforms: [],
-        time: new Date(),
+        updateUiTimestamp: new Date(),
     };
 
     componentDidMount() {
-        document.addEventListener("visibilitychange", this.onFocus, false);
-
+        document.addEventListener("visibilitychange", this.onFocus);
         this.getDepartures();
+        this.setupIntervals();
+    }
 
+    componentWillUnmount() {
+        document.removeEventListener("visibilitychange", this.onFocus);
+        this.clearIntervals();
+    }
+
+    setupIntervals = () => {
         this.fetchDeparturesInterval = setInterval(
             this.getDepartures,
             FETCH_DEPARTURES_INTERVAL,
         );
-        this.updateDeparturesInterval = setInterval(
-            () => this.setState({ time: new Date() }),
+
+        this.updateUiInterval = setInterval(
+            () => this.setState({ updateUiTimestamp: new Date() }),
             UPDATE_UI_INTERVAL,
         );
     }
 
-    componentWillUnmount() {
-        window.removeEventListener("visibilitychange", this.onFocus);
-
+    clearIntervals = () => {
         clearInterval(this.fetchDeparturesInterval);
-        clearInterval(this.updateDeparturesInterval);
+        clearInterval(this.updateUiInterval);
     }
 
     onFocus = () => {
-        if (document.visibilityState === "hidden") {
-            console.log("hidden");
-        } else if (document.visibilityState === "visible") {
-            console.log("visible");
+        if (document.visibilityState === "visible") {
+            this.getDepartures();
+            this.setupIntervals();
+        } else if (document.visibilityState === "hidden") {
+            this.clearIntervals();
         }
     }
 
     getDepartures = async () => {
+        // TODO: Add error handling
+        this.setState({ isLoading: true });
         const response = await fetchDepartures("NSR:StopPlace:58195");
 
         this.setState({
@@ -79,14 +89,15 @@ class Departures extends React.PureComponent<{}, IDeparturesState> {
     }
 
     render() {
-        if (this.state.isLoading) {
-            return <h1>laster...</h1>;
+        if (this.state.isLoading && this.state.platforms.length === 0) {
+            return (
+                <Container>
+                    <h1>Laster...</h1>;
+                </Container>
+            );
         }
 
-        const platforms = this.state.platforms.map(platform => (
-            <Platform key={platform.name} platform={platform} />
-        ));
-
+        const platforms = this.state.platforms.map(platform => <Platform key={platform.name} platform={platform} />);
         const lastUpdated = this.state.lastUpdated
             ? moment(this.state.lastUpdated).format("HH:mm:ss")
             : "aldri";
